128828da94380ddf020a7b7f2c6d92a87805840d
James Halliday <mail@substack.net>
2013-01-19 09:13:17 -0800

    blarg, fix tests

--- test/article/compare.js
+++ test/article/compare.js
@@ -2,7 +2,7 @@ module.exports = function (expected, element, cb) {
     for (var i = 0; i < expected.length; i++) {
         var a = fudge(expected[i]);
         var b = fudge(element.innerHTML);
-        if (a === b || i === expected.length - 1) cb(a, b);
+        if (a === b || i === expected.length - 1) return cb(a, b);
     }
 };
 
