e4d4bebdf5dbc880da7f8e1483532dc584ff8400
James Halliday <mail@substack.net>
2013-05-09 02:13:27 -0700

    article test passes in node

---
+++ index.js
@@ -0,0 +1,49 @@
+var trumpet = require('trumpet');
+var ent = require('ent');
+var concat = require('concat-stream');
+
+module.exports = function (html, params) {
+    var tr = trumpet();
+    Object.keys(params).forEach(function (key) {
+        var val = params[key];
+        
+        tr.select(key, function (node) {
+            if (!val) return;
+            
+            if (typeof val === 'object' && val._html !== undefined) {
+                var copy = shallowCopy(val);
+                delete copy._html;
+                node.update(function (html) {
+                    return val._html;
+                }, Object.keys(copy).length ? copy : undefined);
+            }
+            else if (typeof val === 'object' && val._text !== undefined) {
+                var copy = shallowCopy(val);
+                delete copy._text;
+                node.update(
+                    ent.encode(val._text),
+                    Object.keys(copy).length ? copy : undefined
+                );
+            }
+            else {
+                node.update(ent.encode(val));
+            }
+        });
+    });
+    
+    var body = '';
+    tr.pipe(concat(function (err, src) { body = src }));
+    tr.end(html);
+    return {
+        outerHTML: body,
+        innerHTML: body
+    };
+};
+
+function shallowCopy (obj) {
+    var res = {};
+    Object.keys(obj).forEach(function (key) {
+        res[key] = obj[key];
+    });
+    return res;
+}
--- test/article.js
+++ test/article.js
@@ -1,4 +1,4 @@
-var test = require('tape');
+var test = require('tap').test;
 
 var hyperglue = require('../');
 var html = require('./article/html');
