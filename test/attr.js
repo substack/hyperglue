6ea61deb5ab297e257307408ce5d4cb8edbb82b5
James Halliday <mail@substack.net>
2013-05-13 21:31:38 -0700

    failing test that resets the existing attributes

---
+++ test/attr.js
@@ -0,0 +1,16 @@
+var test = require('tap').test;
+var hyperglue = require('../');
+
+var html = '<img class="a">';
+
+test('attr', function (t) {
+    t.plan(1);
+    
+    var res = hyperglue(html, { 'img.a': { src: '/a.png' } }).innerHTML;
+    
+    t.ok(
+        res === '<img class="a" src="/a.png">'
+        || res === '<img src="/a.png" class="a">',
+        'not ok: ' + res
+    );
+});
