b4c0103cc29f5ff40fe225f3a3e49e8ed4b22839
James Halliday <mail@substack.net>
2013-05-20 20:52:22 -0700

    put browser test in the right place

---
+++ test/browser/steps.js
@@ -0,0 +1,25 @@
+var hyperglue = require('../../');
+var test = require('tape');
+
+test(function (t) {
+    t.plan(4)
+    t.equal(
+        hyperglue('<div></div>',{'div':{_text:'foobar'}}).outerHTML,
+        hyperglue('<div>foobar</div>').outerHTML
+    );
+    t.equal(
+        hyperglue('<div><p></p></div>',{'div':{_text:'foobar'}}).outerHTML,
+        hyperglue('<div>foobar</div>').outerHTML
+    );
+    t.equal(
+        hyperglue(
+            '<div><span><b></b></span></div>',
+            {'div span':{_text:'foobar'}}
+        ).outerHTML,
+        hyperglue('<div><span>foobar</span></div>').outerHTML
+    );
+    t.equal(
+        hyperglue('<div><p></p></div>',{'div p':{_text:'foobar'}}).outerHTML,
+        hyperglue('<div><p>foobar</p></div>').outerHTML
+    );
+});
